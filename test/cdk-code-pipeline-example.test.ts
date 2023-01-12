e850a33cc09f34533b7bb0a3aae9d73c2c20d567
Vinod Patidar <pvinod813@gmail.com>
2023-01-12 17:51:34 +0530

    Initital Commit

---
+++ test/cdk-code-pipeline-example.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkCodePipelineExample from '../lib/cdk-code-pipeline-example-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-code-pipeline-example-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkCodePipelineExample.CdkCodePipelineExampleStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
